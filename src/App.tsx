ffc089e4e0a404c54c117b4595ddd0a86f0bd083
Florian Thiévent <florian.thievent@sbb.ch>
2022-11-11 16:58:30 +0100

    reinit app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import {
+  Alignment,
+  Button,
+  Classes,
+  Navbar,
+  NavbarDivider,
+  NavbarGroup,
+  NavbarHeading,
+  Icon
+} from '@blueprintjs/core'
+import { APP_ICON, APP_NAME } from './Config'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="content">
+      <Navbar>
+        <NavbarGroup align={Alignment.LEFT}>
+          <NavbarHeading>
+            <Icon icon={APP_ICON} className='hdz-app-icon'/>
+            {APP_NAME}
+            </NavbarHeading>
+          <NavbarDivider />
+          <Button className={Classes.MINIMAL} icon="home" text="Home" />
+          <Button className={Classes.MINIMAL} icon="document" text="Files" />
+        </NavbarGroup>
+      </Navbar>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/Config.ts
@@ -0,0 +1,2 @@
+export const APP_NAME = 'Handz'
+export const APP_ICON = 'hand'
